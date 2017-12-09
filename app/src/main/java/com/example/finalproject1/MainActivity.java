f2c414af4677491ccd9055a583927bc53cd01113
PavelBarik <barantsev020@mail.ru>
2017-12-09 15:10:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/finalproject1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.finalproject1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+public class MainActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView1);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year,
+                                            int month, int dayOfMonth) {
+                int mYear = year;
+                int mMonth = month;
+                int mDay = dayOfMonth;
+                String selectedDate = new StringBuilder().append(mMonth + 1)
+                        .append("-").append(mDay).append("-").append(mYear)
+                        .append(" ").toString();
+
+
+            }
+        });
+    }
+}
